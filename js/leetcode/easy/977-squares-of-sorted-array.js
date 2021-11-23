6ac9fa5c752ff7299f944762c9ead2e9e1289c3b
Renyi Tan <tanrenyi@gmail.com>
2021-11-23 10:24:01 +0800

    algorithms

---
+++ js/leetcode/easy/977-squares-of-sorted-array.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/squares-of-a-sorted-array/
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  for (let i = 0; i < nums.length; i++) {
+    nums[i] = Math.pow(nums[i], 2);
+  }
+  return nums.sort((a, b) => a - b)
+};
+
+console.log(sortedSquares([-4, -1, 0, 3, 10])); // [0, 1, 9, 16, 100];
