60e4d9a2888d9e994f7fac659a94677a4d354c49
Renyi Tan <tanrenyi@gmail.com>
2021-11-25 11:52:04 +0800

    reverse string

---
+++ js/leetcode/344-reverse-string.js
@@ -0,0 +1,22 @@
+//  https://leetcode.com/problems/reverse-string/
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function (s) {
+  let i = 0;
+  let j = s.length-1;
+
+  while (i < j) {
+    let temp = s[i];
+    s[i] = s[j]
+    s[j] = temp
+    i++;
+    j--;
+  }
+  return s
+};
+
+console.log(reverseString(['h', 'e', 'l', 'l', 'o'])); //["o","l","l","e","h"]
+console.log(reverseString(['h', 'e', 'l', 'o'])); //["o","l","e","h"]
