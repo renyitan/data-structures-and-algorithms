01a87bd7a73ac573f5d48e450ac95f6cd3614c97
Renyi Tan <renyi.tan@gatech.edu>
2022-03-07 01:13:06 +0800

    finally added

---
+++ python/practice/ctci/chp04_trees_and_graphs/p03_list-of-depths.py
@@ -0,0 +1,12 @@
+from ds.binary_tree import BinaryTree
+if __name__ == "__main__":
+    # pass
+
+    bt = BinaryTree()
+    bt.add(5)
+    bt.add(2)
+    bt.add(6)
+    bt.add(1)
+    bt.add(3)
+    bt.add(4)
+    bt.show()
