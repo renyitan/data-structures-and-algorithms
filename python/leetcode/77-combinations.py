c191c766010ef31e150ae90e0c5eee172015a265
Renyi Tan <tanrenyi@gmail.com>
2022-04-05 11:36:54 +0800

    ;leetcoder

--- python/leetcode/77-combinations.py
+++ python/leetcode/77-combinations.py
@@ -1,18 +1,18 @@
-
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
-
-        def backtrack(start, path):
-            if len(path) == k:
-                result.append(path.copy())
-                return
-
-            for i in range(start, n+1):
-                path.append(i)
-                backtrack(i+1, path)
-                path.pop()
-
-        backtrack(1, [])
-
-        return result
+        output = []
+        def backtrack(first=1, curr=[]):
+            # if combination is done
+            if len(curr) == k:
+                output.append(curr.copy())
+            
+            for i in range(first, n+1):
+                curr.append(i)
+                backtrack(i+1, curr)
+                curr.pop()
+            
+        backtrack()
+        return output
+                
+            
+            
