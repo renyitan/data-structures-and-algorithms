9f4c4fd4e3e4e1d865f666367675133b00491d6d
Renyi Tan <renyi.tan@gatech.edu>
2022-04-21 12:05:41 +0800

    leetcoder

---
+++ c#/leetcode/832-flipping-an-image.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[][] FlipAndInvertImage(int[][] image) {
+        // flip image
+        for (int i=0
+             i < image.Length
+             i++){
+            image[i] = image[i].Reverse().ToArray()
+        }
+        for (int i=0
+             i < image.Length
+             i++) {
+            for (int j=0
+                 j < image[0].Length
+                 j++) {
+                image[i][j] = Convert.ToBoolean(image[i][j]) ? 0: 1
+            }
+        }
+
+        return image
+    }
+}
