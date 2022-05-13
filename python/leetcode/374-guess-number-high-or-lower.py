5ba7a982b9717162983ececc13deda4b594dcff8
Renyi Tan <tanrenyi@gmail.com>
2022-05-13 16:53:01 +0800

    leetcoder

--- python/leetcode/374-guess-number-high-or-lower.py
+++ python/leetcode/374-guess-number-high-or-lower.py
@@ -8,13 +8,12 @@
 class Solution:
     def guessNumber(self, n: int) -> int:
         left, right = 1, n
-        
+
         while left <= right:
-            mid = (left+right) //2
+            mid = (left+right) // 2
             if guess(mid) == 0:
                 return mid
             elif guess(mid) > 0:
                 left = mid + 1
             else:
                 right = mid-1
-        
